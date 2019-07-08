b6ec0be0819148e0d28890d11c3f9c3396d0efcc
yuuhi-s <soejima0303@gmail.com>
2019-07-08 21:40:29 +0900

    カメラの起動から検出まで行えるようにpyファイルを変更

--- detection.py
+++ detection.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from time import sleep
 
 
 class detection:
@@ -7,7 +8,6 @@ class detection:
     検出を行うクラス
     Parameters
     --------------
-    img : 入力画像
     cvsize : 切り取るサイズ
     margin : マージン
     
@@ -16,8 +16,6 @@ class detection:
     default : デフォルト画像
     preimg : 1つ前の画像
     '''
-    
-    
     def __init__(self, cvsize=(224, 224), margin=10):
         
         #ハイパーパラメータ
@@ -82,29 +80,29 @@ class detection:
         ---------
         detected_image : 検出した結果のnumpy配列
         """
+        #検出範囲
+        rangethreshold = int(self.cvsize[0] / 10)
+    
         #デフォルト画像
         if self.default is None:
             self.default = self._get_frame(img)
-            
+
         #1つ前の画像の初期値
         if self.preimg is None:
             self.preimg = self._get_frame(img)
 
-        #検出範囲
-        rangethreshold = int(self.cvsize[0] / 10)
-
         #1つ前の画像との背景差分の取得
         gbur, frame = self._get_frame(img, frame_return=True)
         fgmask = self._get_background_subtraction(self.preimg, gbur)
-        
+
         #preimgの更新
         self.preimg = gbur
-        
+
         #前の画像と変化なし
         if np.max(fgmask) < 1:
             #デフォルト画像との背景差分の取得
             fgmask = self._get_background_subtraction(self.default, gbur)
-            
+
             #デフォルトと変化あり
             if np.max(fgmask) > 200:
                 #変化がある位置を抜き出す
@@ -134,23 +132,54 @@ class detection:
                 if y + h > frame.shape[0]:
                     h = h - y + h - frame.shape[0]
 
-                #物体が検出範囲以上であれば検出
+                #物体が検出範囲以上であれば検出する
                 if w > rangethreshold and h > rangethreshold:
-                    print("検出成功　　座標　x:{} y{} w {} h {}".format(x, y, w, h))
                     detected_image = frame[y: y + h, x: x + w]
-                    print("detected_image取得")
-                    
                     return detected_image
 
-                #物体が検出範囲より小さければ検出しない
                 else:
-                    print('検出できていません')
                     pass
-                
             else:
-                print('検出できていません')
                 pass
+        else:
+            pass
+
+            
+def main():
+    '''
+    検出を実行する関数
+    '''
+    #各クラスをインスタンス化
+    detecter = detection()
+    
+    #カメラ起動
+    cap = cv2.VideoCapture(0)
+    sleep(1)
+    
+    #検出    
+    while True:
+        ret, flame = cap.read()
+        cv2.imshow('検出中', flame)
+        if ret:
+            detected_image = detecter.object_detection(flame)
+                
+            #検出完了したらbreak
+            if detected_image:
+                print('検出されました')
+                break
             
         else:
-            print('検出できていません')
-            pass
+            print('検出できませんでした')
+            break
+            
+        #'Q'が押されるとbreak
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            print('終了ボタンが押されました')
+            break
+                
+    #検出結果の出力
+    if detected_image:
+        cv2.imshow('検出結果', detected_image)
+    cap.release()
+    cv2.destroyWindow('検出中')
