703eb988ee79097e46c6d29d14ea90d8c57a2cfa
raxman0721 <address.akirakira@gmail.com>
2019-07-10 22:50:16 +0900

    make hand augmentation

--- make_image.py
+++ make_image.py
@@ -1,5 +1,6 @@
 import cv2
 import detection
+from time import sleep
 
 if __name__ == '__main__':
     detecter1 = detection.Detection()
@@ -10,7 +11,7 @@ if __name__ == '__main__':
     #検出タスク
     count = 1
     while True:
-        sleep(0.2)
+        sleep(0.001)
         ret, flame = cap.read()
         cv2.imshow('scan_Running',flame)
 
@@ -21,8 +22,8 @@ if __name__ == '__main__':
             #検出完了したら出力へ
             if detected_image is not None:
                 print('撮影{}回目'.format(count))
-                cv2.imwrite('./image3/cclemon/cclemon__' + str(count) + '.jpg', img)
+                cv2.imwrite('./image3/fanta_litchi/fanta_litchi__' + str(count) + '.jpg', detected_image)
                 count += 1
 
-                if count == 10:
+                if count == 100:
                     break
