8a3efcaf045590566d4affe6695a499226b74b3b
yuuhi-s <soejima0303@gmail.com>
2019-07-10 13:47:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,24 @@
 import detection
 import classify
 import cv2
-#main.pyの中で必要な関数を定義
+from time import sleep
 
 #if __name__ == __'main'__でここから処理開始
 if __name__ == '__main__':
     # クラスがインスタンス化
     detecter = detection.Detection()
-    claster = classify.classify()
-    cap = cv2.VideoCapture(0)
-
+    claster = classify.predict_class()
+    
+    #初期値
     cart = []
     amount = 0
+    cart_loop = True
     pet_dict = {'アクエリ':140, 'ソーダフロート':150, 'cc レモン':160, 'ファンタ':170}
     pet_lict = ['アクエリ', 'ソーダフロート', 'cc レモン', 'ファンタ']
 
-
-    while True:
+    #1回の買い物
+    while cart_loop == True:
+        cap = cv2.VideoCapture(0)
         print('検出開始します。')
 
         #検出タスク
@@ -35,6 +37,19 @@ if __name__ == '__main__':
                     print('scan_Successed')
                     cv2.destroyWindow('scan_Running')
                     cap.release()
+                    
+                    #検出結果の出力
+                    cv2.imshow('Result', detected_image)
+
+                    label = claster.predict(detected_image)
+
+                    #今はラベルが帰って来てるけど、最終的には各クラスの確率を返す関数として、main.pyにてlabel付する。
+                    #label = pet_list[np.argmax(claster.predict_multi_class(detected_image))]
+
+                    cart.append(label)
+                    print('商品は{}:150円です。'.format(label))
+                    print('これでお買い物終了の場合はqを押してください。\nまだ商品がある場合は再度検出ボックスに商品を入れてください。')
+
                     break
 
             #失敗
@@ -46,24 +61,8 @@ if __name__ == '__main__':
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
                 print('Pressed finish button')
+                cart_loop = False
                 break
-
-        #検出結果の出力
-        cv2.imshow('Result', detected_image)
-
-        label = claster.predict_multi_class(detected_image)
-
-        #今はラベルが帰って来てるけど、最終的には各クラスの確率を返す関数として、main.pyにてlabel付する。
-        #label = pet_list[np.argmax(claster.predict_multi_class(detected_image))]
-
-        cart.append(label)
-        print('商品は{}:150円です。'.format(label))
-        print('これでお買い物終了の場合はqを押してください。/nまだ商品がある場合は再度検出ボックスに商品を入れてください。')
-        #'a'が押されるとbreak
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('a'):
-            print('Pressed finish button')
-            break
 
     # cart内商品の合計金額を出す。
     for pet in cart:
