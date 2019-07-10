ef1c575151ba202c5a337f5c6e63e8673e8279d2
yuuhi-s <soejima0303@gmail.com>
2019-07-10 20:47:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,13 +19,12 @@ if __name__ == '__main__':
     cart_num = {'namacha':0, 'soda_float':0, 'cclemon':0, 'fanta_litchi':0, 'cocacola':0}
     pet_lict = ['namacha', 'soda_float', 'cclemon', 'fanta_litchi', 'cocacola']
 
-    #stand machine
+    #レジの立ち上げ
     while True:
-        
-        #1回の買い物
+        #買い物開始
         while cart_loop == True:
             cap = cv2.VideoCapture(0)
-            print('検出開始します。')
+            print('検出を開始します。')
 
             #検出タスク
             while True:
@@ -37,13 +36,13 @@ if __name__ == '__main__':
                 if ret:
                     detected_image = detecter1.object_detection(flame)
 
-                    #検出完了したらbreak
+                    #検出完了したら出力へ
                     if detected_image is not None:
-                        print('scan_Successed')
+                        print('スキャンに成功しました')
                         cv2.destroyWindow('scan_Running')
                         cap.release()
-                        
-                        #predict label
+
+                        #商品を予測
                         label = claster.predict(detected_image)
 
                         #今はラベルが帰って来てるけど、最終的には各クラスの確率を返す関数として、main.pyにてlabel付する。
@@ -67,9 +66,10 @@ if __name__ == '__main__':
                             else:
                                 pass
 
-                        # cart内商品の合計金額を出す。
+                        #カート内商品の合計金額を出す。
                         print('合計金額 : {}\n'.format(sum(amount)))
-                        print('この商品を取り消す場合は「r」を押してください\n'\
+                        print('商品を取り出してください\n'\
+                              '直前の商品を取り消す場合は「r」を押してください\n'\
                               'これでお買い物終了の場合は「q」を押してください。\n'\
                               'まだ商品がある場合は再度検出ボックスに商品を入れてください。\n')
 
@@ -81,27 +81,32 @@ if __name__ == '__main__':
                             non_detected_image = detecter2.object_detection(flame)
                             if non_detected_image is not None:
                                 cap2.release()
-                                break
+                                break #直前のループ
                             else:
                                 continue
-                        break
+                        break #検出のbreak
+
+                    #検出できなかったら無視
+                    else:
+                        pass
 
                 #失敗
                 else:
-                    print('scan_Failured')
+                    print('スキャンに失敗しました')
                     continue
 
-                #'q'が押されるとbreak
+                #'q'が押されると会計
                 key = cv2.waitKey(1) & 0xFF
                 if key == ord('q'):
-                    print('Pressed finish button')
+                    #買い物ループを止める
                     cart_loop = False
                     # cart内商品の合計金額を出す。
                     cap.release()
                     cv2.destroyWindow('scan_Running')
+                    print('お会計')
                     print('合計金額は{}円です。しっかり払えや。\n'.format(sum(amount)))
-                    print('please push e or s')
-                    break
+                    print('「s」を押すと会計開始\n'\
+                          '「e」を押すとシステム終了')
 
                 #'r'が押されると一つ前の商品を抜く
                 elif key == ord('r'):
@@ -112,12 +117,13 @@ if __name__ == '__main__':
                     print('検出を開始します')
                     continue
 
-        key = cv2.waitKey(1) & 0xFF
-        #push 'e' end
-        if key == ord('e'):
+        key2 = cv2.waitKey(1) & 0xFF
+        #'e'が押されるとレジの終了
+        if key2 == ord('e'):
+            print('システムが終了しました')
             break
-        
-        #push 's' back to start
-        elif key == ord('s'):
+
+        #'s'が押されると検出開始
+        elif key2 == ord('s'):
             cart_loop = True
             continue
